d3b5796c65ec085f6c5c63c0dd7d531f26ad01b1
Marc Bell <marc.bell@sap.com>
2015-02-26 10:16:53 -0800

    ->Fixed bug that was not causing the Add Button to show and create topics.

--- OpenSocial/Gadget/Tutorial/Tutorial_04_Adding_topics/adding_topics.js
+++ OpenSocial/Gadget/Tutorial/Tutorial_04_Adding_topics/adding_topics.js
@@ -68,8 +68,8 @@ function renderInfo() {
     document.getElementById('body').innerHTML = html;
     
     /** Create "Add topic" button to the footer */
-    //html += '<input type="text" id="textBox" value=""/><button id="addInput" onclick="addInput()">Add Topic</button>'
-    //document.getElementById('footer').innerHTML = html;
+    html += '<input type="text" id="textBox" value=""/><button id="addInput" onclick="addInput()">Add Topic</button>';
+    document.getElementById('footer').innerHTML = html;
     
     /** Adjust window size dynamically */
     gadgets.window.adjustHeight();
